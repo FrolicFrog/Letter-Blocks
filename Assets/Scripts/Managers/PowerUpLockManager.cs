d1b9d15153759ebb37b8ef201b998841a2817a95
MinerJi <Vibhoosahu@gmail.com>
2026-09-03 20:47:35 +0530

    Ads  Button

--- Assets/Scripts/Managers/PowerUpLockManager.cs
+++ Assets/Scripts/Managers/PowerUpLockManager.cs
@@ -35,17 +35,25 @@ public class PowerUpLockManager : MonoBehaviour
                     lockedObjectDict[lvl].Value.SetActive(true);
                     if(LevelManager.Instance.CurLevelNumber == lvl)
                     {
-                        lockedObjectDict[lvl].Key.GetComponentInParent<UIElementScaler>().StartScaling();
+                        lockedObjectDict[lvl].Key.GetComponentInParent<UIElementScaler>(true).StartScaling();
                         panel.SetActive(true);
                     }
                 }
-                if (lockedObjectDict[lvl].Value.GetComponentInChildren<TextMeshProUGUI>().text == "0")
+                if (lockedObjectDict[lvl].Value.GetComponentInChildren<TextMeshProUGUI>(true).text == "0")
                 {
-                    lockedObjectDict[lvl].Key.GetComponentInParent<Toggle>().interactable = false;
+                    lockedObjectDict[lvl].Key.GetComponentInParent<Toggle>(true).interactable = false;
+                   
+                    lockedObjectDict[lvl].Value.SetActive(false);
+                    lockedObjectDict[lvl].Value.transform.parent.GetChild(1).gameObject.SetActive(false);
+                    lockedObjectDict[lvl].Value.transform.parent.GetChild(2).gameObject.SetActive(true);
                 }
                 else
                 {
-                    lockedObjectDict[lvl].Key.GetComponentInParent<Toggle>().interactable = true;
+
+                    lockedObjectDict[lvl].Value.SetActive(true);
+                    lockedObjectDict[lvl].Key.GetComponentInParent<Toggle>(true).interactable = true;
+                    lockedObjectDict[lvl].Value.transform.parent.GetChild(1).gameObject.SetActive(true);
+                    lockedObjectDict[lvl].Value.transform.parent.GetChild(2).gameObject.SetActive(false);
                 }
             }
             
@@ -56,6 +64,11 @@ public class PowerUpLockManager : MonoBehaviour
     public void UpdatePowerUpQuantity(int key,int quantity)
     {
         PlayerPrefs.SetInt(key.ToString(), PlayerPrefs.GetInt(key.ToString(), 2)+quantity);
+        lockedObjectDict[key].Value.GetComponentInChildren<TextMeshProUGUI>().text = PlayerPrefs.GetInt(key.ToString(), 2).ToString();
+    }
+    public void UpdatePowerUpQuantity(int key)
+    {
+        PlayerPrefs.SetInt(key.ToString(), PlayerPrefs.GetInt(key.ToString(), 2) +1);
         lockedObjectDict[key].Value.GetComponentInChildren<TextMeshProUGUI>().text = PlayerPrefs.GetInt(key.ToString(), 2).ToString();
     }
 }
